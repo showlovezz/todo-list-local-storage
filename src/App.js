29ca5c96df7e1d36c47a4a2afcc32908bb309b17
Vita Ora <vitata710@gmail.com>
2022-04-07 15:39:04 +0800

    Add Header component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
+
 import Layout from './components/Layout';
 import Card from './components/Card';
-
+import Header from './components/Header';
 
 const App = () => {
   return (
     <Layout>
       <Card>
+        <Header />
         <h1>Hello!</h1>
+        list here !
       </Card>
     </Layout>
   );
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className='bg-primary text-white text-center rounded p-3'>
+      <h3 className='mb-0'>Todo App - By Vita Ora</h3>
+    </header>
+  )
+}
+export default Header
