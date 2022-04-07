27f5d395320df3845649aa05cc8c69da0e3d7752
Vita Ora <vitata710@gmail.com>
2022-04-07 11:25:25 +0800

    Add react、react-bootstrap package and setting scss

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import * as ReactDOMClient from 'react-dom/client';
-import './index.css';
+
 import App from './App';
+import './index.scss';
 
 const container = document.getElementById('root');
 
