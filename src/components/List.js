1b54d5f8c1163b3cd6c3731a73d30e14821c3052
Vita Ora <vitata710@gmail.com>
2022-04-08 15:07:44 +0800

    Add no todo list component

--- src/components/List.js
+++ src/components/List.js
@@ -1,16 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types'
-import {ListGroup} from 'react-bootstrap'
+import {ListGroup, Alert} from 'react-bootstrap'
 
 import ListItem from './ListItem';
 
 const List = ({del, done, todoList}) => {
+  if (todoList.length === 0) {
+    return (
+      <Alert variant='secondary' className='text-center mb-0'>
+        <Alert.Heading className='text-danger'>No Todo Found !</Alert.Heading>
+        <p className='mb-0'>Add your own todo !</p>
+      </Alert>
+    )
+  }
+
   return (
     <ListGroup>
       {
+        todoList &&
         todoList.map((todo) => {
           return (
-            <ListItem key={todo.id} id={todo.id} title={todo.title} delHandler={del} doneHandler={done} done={todo.done} />
+            <ListItem
+              key={todo.id}
+              id={todo.id}
+              title={todo.title}
+              delHandler={del}
+              doneHandler={done}
+              done={todo.done}
+            />
           )
         })
       }
