9de590f06d722dfa70f0b25512166bde60a11fad
Vita Ora <vitata710@gmail.com>
2022-04-12 09:38:42 +0800

    Adjust app file code style

--- src/App.js
+++ src/App.js
@@ -6,10 +6,8 @@ import Header from './components/Header';
 import List from './components/List';
 import Form from './components/Form';
 
-const App = () => {
-  const [todo, setTodo] = useState('')
+const useTodoList = () => {
   const [todoList, setTodoList] = useState([])
-  const [error, setError] = useState('')
 
   useEffect(() => {
     const getTodoList = JSON.parse(localStorage.getItem('todoList'))
@@ -23,22 +21,34 @@ const App = () => {
     localStorage.setItem('todoList', JSON.stringify(todoList))
   }, [todoList])
 
-  const delHandler = (todoId) => {
+  return [todoList, setTodoList];
+}
+
+// how react handle shared logic best practice
+// mixin, HOC, render props, hooks
+
+const App = () => {
+  const [todo, setTodo] = useState('')
+  const [error, setError] = useState('')
+  const [todoList, setTodoList] = useTodoList()
+
+  const handleDelete = (todoId) => {
     if (window.confirm('Are you sure ?')) {
       setTodoList((todoList) => todoList.filter((todo) => todo.id !== todoId))
     }
   }
-  const doneHandler = (todoId) => {
-    const index = todoList.findIndex((item) => item.id === todoId)
-    const duplicateTodoList = [...todoList]
+  const handleDone = (todoId) => {
+    // const index = todoList.findIndex((item) => item.id === todoId)
+    // const duplicateTodoList = [...todoList]
 
-    duplicateTodoList[index] = {
-      id: todoList[index].id,
-      title:  todoList[index].title,
-      done: !todoList[index].done,
-    }
+    // duplicateTodoList[index] = {
+    //   id: todoList[index].id,
+    //   title:  todoList[index].title,
+    //   done: !todoList[index].done,
+    // }
 
-    setTodoList(duplicateTodoList)
+    // setTodoList(duplicateTodoList)
+    setTodoList((list) => list.map(todo => todo.id !== todoId ? todo : {...todo, done: !todo.done}))
   }
   const submitHandler = (e) => {
     e.preventDefault();
@@ -58,7 +68,7 @@ const App = () => {
       <Card>
         <Header />
         <Form todo={todo} change={(e) => setTodo(e.target.value)} submit={submitHandler} error={error} />
-        <List del={delHandler} done={doneHandler} todoList={todoList} />
+        <List del={handleDelete} done={handleDone} todoList={todoList} />
       </Card>
     </Layout>
   );
